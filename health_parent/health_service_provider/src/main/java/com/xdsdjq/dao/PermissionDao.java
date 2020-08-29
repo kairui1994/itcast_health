5dbb269f30072056161fd9d6bc6225d64954fde3
Feifei <1246789957@qq.com>
2020-08-29 16:25:48 +0800

    权限功能

--- health_parent/health_service_provider/src/main/java/com/xdsdjq/dao/PermissionDao.java
+++ health_parent/health_service_provider/src/main/java/com/xdsdjq/dao/PermissionDao.java
@@ -1,5 +1,6 @@
 package com.xdsdjq.dao;
 
+import com.github.pagehelper.Page;
 import com.xdsdjq.pojo.Permission;
 
 import java.util.List;
@@ -10,4 +11,16 @@ public interface PermissionDao {
     Set<Permission> findPermissionByRoleId(Integer roleId);
 
     List<Permission> findAll();
+
+    Page<Permission> selectByCondition(String queryString);
+
+    void add(Permission permission);
+
+    Permission findById(int id);
+
+    void edit(Permission permission);
+
+    Long findRoleAndPermissionById(int id);
+
+    void deleteById(int id);
 }
--- health_parent/health_service_provider/src/main/java/com/xdsdjq/service/impl/PermissionServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/xdsdjq/service/impl/PermissionServiceImpl.java
@@ -1,12 +1,18 @@
 package com.xdsdjq.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.xdsdjq.dao.PermissionDao;
+import com.xdsdjq.entity.PageResult;
+import com.xdsdjq.entity.QueryPageBean;
 import com.xdsdjq.pojo.Permission;
 import com.xdsdjq.service.PermissionService;
+import com.xdsdjq.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service(interfaceClass = PermissionService.class)
@@ -17,5 +23,46 @@ public class PermissionServiceImpl implements PermissionService {
     @Override
     public List<Permission> findAll() {
         return permissionDao.findAll();
+    }
+
+    @Override
+    public PageResult pageQuery(QueryPageBean queryPageBean) {
+        Integer currentPage = queryPageBean.getCurrentPage();
+        Integer pageSize = queryPageBean.getPageSize();
+        String queryString = queryPageBean.getQueryString();
+
+        //使用分页助手
+        PageHelper.startPage(currentPage, pageSize);
+        Page<Permission> page = permissionDao.selectByCondition(queryString);
+        long total = page.getTotal();
+        List<Permission> rows = page.getResult();
+        return new PageResult(total, rows);
+    }
+    @Override
+    public void add(Permission permission) throws Exception {
+        Date date = new Date();
+        permission.setCreationdate(DateUtils.parseDate2String(date));
+        permissionDao.add(permission);
+    }
+
+    @Override
+    public Permission findById(int id) {
+        return permissionDao.findById(id);
+    }
+
+    @Override
+    public void edit(Permission permission) {
+        permissionDao.edit(permission);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Long num = permissionDao.findRoleAndPermissionById(id);
+        if (num > 0) {
+            //不等于0表示已经关联角色 抛出异常
+            System.out.println(3 / 0);
+        } else {
+            permissionDao.deleteById(id);
+        }
     }
 }
