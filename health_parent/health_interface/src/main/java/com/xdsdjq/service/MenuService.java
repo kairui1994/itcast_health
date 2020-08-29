26a1e4ac4b7de072cfe1c5799b848d48bb4dfff5
hq0419 <ahmashq95@gmail.com>
2020-08-29 19:36:23 +0800

    修改o

--- health_parent/health_interface/src/main/java/com/xdsdjq/service/MenuService.java
+++ health_parent/health_interface/src/main/java/com/xdsdjq/service/MenuService.java
@@ -1,7 +1,11 @@
 package com.xdsdjq.service;
 
+import com.xdsdjq.entity.PageResult;
+import com.xdsdjq.entity.QueryPageBean;
 import com.xdsdjq.pojo.Menu;
 
 public interface MenuService {
     Menu findMenuById(Integer id);
+
+    PageResult pageQuery(QueryPageBean queryPageBean);
 }
